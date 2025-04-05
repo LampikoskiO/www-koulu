d286a2b4b3a1f5d944a0f5562bbef3733582ddb5
BiodiversityConservationToken <biodiversityconservationtoken@gmail.com>
2025-04-05 15:15:52 +0300

    template engine setup

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ import catRoutes from "./routes/catRoutes.js";
 
 dotenv.config();
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 const app = express();
 const port = process.env.PORT || 3000;
 
